8674ebfbed16d0e88d20b8c832b85c1769895ba6
Gabriel Oquendo <oquendogabriel18@gmail.com>
2024-10-27 21:08:35 -0500

    Refactor page.js to use Redux store for login functionality

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,118 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useDispatch, useSelector } from "react-redux";
 
-export default function Home() {
+import {
+  LoginEmployee,
+  getEmployee,
+} from "./store/features/employee/employeeThunks";
+import { getToken } from "../utils/getToken";
+export default function Login() {
+  const dispatch = useDispatch();
+  const { employee, loading, error } = useSelector((state) => state.employee);
+
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+
+  const validateForm = () => {
+    const regesEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    const errors = {};
+    if (!email || !regesEmail.test(email)) {
+      errors.email = "error";
+    }
+    if (!password) {
+      errors.password = "error";
+    }
+    setErrors(errors);
+    if (errors.email || errors.password) {
+      return false;
+    }
+    return true;
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const isValid = validateForm();
+    if (!isValid) return;
+    const formData = {
+      email,
+      password,
+    };
+    dispatch(LoginEmployee(formData));
+  };
+
+  useEffect(() => {
+    if (employee && employee._id) {
+      router.push("/home");
+    }
+  }, [employee, router]);
+
+  useEffect(() => {
+    if (getToken()) {
+      dispatch(getEmployee());
+    }
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="flex items-center justify-center min-h-screen bg-gray-900 md:px-20 px-0">
+      <div className="bg-gray-800 p-8 rounded-lg shadow-lg lg:w-1/2 2xl:w-1/3 w-full md:px-20">
+        <div className="flex flex-col items-center">
+          <div className="bg-white p-2 rounded-full">
+            <img
+              src="/Logo.png"
+              alt="Logo"
+              className="w-24 h-24 object-contain"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <h2 className="text-white text-lg font-semibold mt-4">
+            Bienvenido a EasyPark
+          </h2>
+          <p className="text-gray-400 text-center mt-1 mb-4">
+            Encuentra y reserva tu parqueadero fácilmente en segundos.
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <form onSubmit={handleLogin} className="flex flex-col space-y-4">
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Ingresa tu correo"
+            className={`bg-gray-700 text-white px-4 py-2 rounded focus:outline-none ${
+              errors.email && "border-red-500 border-2"
+            }`}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Ingresa tu contraseña"
+            className={`bg-gray-700 text-white px-4 py-2 rounded focus:outline-none ${
+              errors.password && "border-red-500 border-2"
+            }`}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <button
+            type="submit"
+            className="bg-lime-500 text-gray-900 font-semibold py-2 rounded mt-2"
+          >
+            {loading ? "Cargando..." : "Iniciar sesión"}
+          </button>
+        </form>
+        {error && <p className="text-red-500 text-center mt-4">{error}</p>}
+        <div className="flex items-center my-4 text-gray-400">
+          <p>
+            ¿Sabías que usar un parqueadero seguro reduce el riesgo de daños y
+            robos? Tu vehículo merece la mejor protección, y nosotros te
+            ofrecemos tranquilidad en cada viaje. Elige la seguridad y disfruta
+            del camino sin preocupaciones.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
